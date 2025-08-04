2bfe75c2df023079837f7adb13f407bf77677b63
HarshWaghela88 <harsh.wghl88@gmail.com>
2025-08-04 14:09:17 +0530

    Add basic calculator methods

---
+++ ConsoleCalculator.java
@@ -0,0 +1,21 @@
+public class ConsoleCalculator {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error: Division by zero is not allowed.");
+            return Double.NaN;
+        }
+        return a / b;
+    }
+}
